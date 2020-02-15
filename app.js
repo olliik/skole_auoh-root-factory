7b2f688b3d6f6c19c55dfcddb3c41e21ca8e770c
olliik <olli.isokoivisto@gmail.com>
2020-02-15 12:45:42 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const robot = require('./robot');
+const view = require('./view');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+console.log('Start robot');
+const fanuc = robot.create();
+fanuc.run();
+
+console.log('Start server');
+app.get('/', (req, res, next) => {
+    res.send(view.status_view({
+        joints: fanuc.joints
+    }));
+});
+
+app.use((req, res, next) => {
+    res.redirect('/');
+});
+
+
+app.listen(PORT);
